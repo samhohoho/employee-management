0d9d1d305f8c083bcf9b5345d6bfc81f016cf721
Hosam <hosamchung998@gmail.com>
2025-02-15 14:52:11 +0800

    add sql script for tables setup

--- postgres_init.sql
+++ postgres_init.sql
@@ -1,1 +1,18 @@
-CREATE DATABASE Employee;
+CREATE TABLE Department (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE Employee (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    department_id INT NOT NULL UNIQUE,
+
+    CONSTRAINT fk_department FOREIGN KEY(department_id) REFERENCES Department(id)
+);
+
+CREATE TABLE Project (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    employee_id INT REFERENCES Employee(id)
+);
