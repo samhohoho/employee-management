31dbd8b5515985202bb7f3ae46db531ba0d56b74
Hosam <hosamchung998@gmail.com>
2025-03-08 14:32:02 +0800

    disable spring-security

--- src/main/java/com/employee/employee_management/config/SecurityConfig.java
+++ src/main/java/com/employee/employee_management/config/SecurityConfig.java
@@ -12,24 +12,25 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-    @Bean
-    SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((auth) -> auth
-                        .anyRequest().authenticated())
-                .httpBasic(Customizer.withDefaults());
-
-        return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails userDetails = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(userDetails);
-    }
+    // @Bean
+    // SecurityFilterChain configure(HttpSecurity http) throws Exception {
+    //     http
+    //             .authorizeHttpRequests((auth) -> auth
+    //                     .anyRequest().authenticated())
+    //             .httpBasic(Customizer.withDefaults())
+    //             .csrf(csrf -> csrf.disable());
+    //
+    //     return http.build();
+    // }
+    //
+    // @Bean
+    // public UserDetailsService userDetailsService() {
+    //     UserDetails userDetails = User.withDefaultPasswordEncoder()
+    //             .username("user")
+    //             .password("password")
+    //             .roles("USER")
+    //             .build();
+    //
+    //     return new InMemoryUserDetailsManager(userDetails);
+    // }
 }
